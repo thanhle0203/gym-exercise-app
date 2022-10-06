1d2b59275d0a89c84b1a20e0a4000e55efeb1649
Thanh Le <thanhlevan0203@gmail.com>
2022-10-05 23:43:01 -0700

    Create HeroBanner component

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,8 +1,26 @@
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <Box sx={{ 
+      mt: { lg: '212px', sx:'70px' },
+      ml: { sm: '50px'}
+      }} position='relative' p='20px'
+    >
+      <Typography color='#FF2625' fontWeight='600' fontSize='26px'>
+        Fitness Club
+      </Typography>
+
+      <Typography>
+        Sweat, Smile <br /> and Repeat
+      </Typography>
+
+      <Typography>
+        Check out the most effective exercises
+      </Typography>
+
+    </Box>
   )
 }
 
