f57c22caa0ec0b5afc293c00f64850077d9a8d4b
Thanh Le <thanhlevan0203@gmail.com>
2022-10-06 00:34:35 -0700

    Styling HeroBanner Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import './App.css';
 import Navbar from './components/Navbar';
 import Footer from "./components/Footer";
 import Home from './pages/Home';
 import ExerciseDetail from './pages/ExerciseDetail';
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import { Box } from '@mui/material';
+
 
 const App = () => {
     return (
--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -8,24 +8,53 @@ const HeroBanner = () => {
       mt: { lg: '212px', sx:'70px' },
       ml: { sm: '50px'}
       }} position='relative' p='20px'
+      flexDirection='row'
     >
-      <Typography color='#FF2625' fontWeight='600' fontSize='26px'>
-        Fitness Club
-      </Typography>
+      <div style={{ alignItems: 'flex-start'}}>
+        <Typography color='#FF2625' fontWeight='600' fontSize='26px'>
+          Fitness Club
+        </Typography>
 
-      <Typography fontWeight={700} 
-        sx={{ fontSize: { lg: '44px', xs: '40px'} }}
+        <Typography fontWeight={700} 
+          sx={{ fontSize: { lg: '44px', xs: '40px'} }}
+          mb="23px" mt="30px"
+        >
+          Sweat, Smile <br /> and Repeat
+        </Typography>
+
+        <Typography fontSize='22px' fontWeight='35px' mb={4}>
+          Check out the most effective exercises
+        </Typography>
+
+        <Button variant='contained' color='error' href='#exercises'
+        sx={{ backgroundColor: '#ff2625',  padding: '10px' }}
+        >
+          Explore Exercises
+        </Button>
+      </div>
+
+      <Typography
+        fontWeight={600}
+        color='#FF2625'
+        sx={{
+          opacity: 0.1,
+          display: { lg: 'block', xs: 'none' }
+        }}
+        fontSize="200px"
+        mt="50px"
       >
-        Sweat, Smile <br /> and Repeat
+        Exercises
       </Typography>
+      
 
-      <Typography fontSize='22px' fontWeight='35px'>
-        Check out the most effective exercises
-      </Typography>
+      <div style={{ alignItems:'flex-end'}}>
+        <img  src='https://redefiningstrength.com/wp-content/uploads/2016/12/workouts.jpg'
+          alt='banner'
+          className='hero-banner-img'
+        />
+      </div>
+      
 
-      <Button variant='contained' color='error'>
-        Explore Exercises
-      </Button>
     </Box>
   )
 }
