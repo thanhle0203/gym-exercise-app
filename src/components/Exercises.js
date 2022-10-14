e8ab511b85e1528fabf962dc400d5f05755fec5a
Thanh Le <thanhlevan0203@gmail.com>
2022-10-13 22:03:59 -0700

    Fetch Data to Exercises component

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,19 +1,29 @@
-import { Typography } from '@mui/material'
+import { Stack, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import React from 'react'
 
 import { exerciseOptions, fetchData } from '../utils/fetchData'
 
 const Exercises = ({ exercises, setExercises, }) => {
+  console.log(exercises);
+  
   return (
     <Box id="exercises"
       sx={{mt: {lg: '100px' }}}
       mt="50px"
       p="20px"
     >
-        <Typography>
-            Exercises
+        <Typography variant='h3' mb='46px'>
+           Showing Exercises
         </Typography>
+
+        <Stack direction='row' sx={{ gap: { lg: '110px', xs: '50px' }}}
+          flexWrap="wrap" justifyContent='center'
+        >
+          {exercises.map((exercises, index) => (
+            <p>{exercises.name}</p>
+          ))}
+        </Stack>
     </Box>
   )
 }
