e10dd5744172581fe11672192e0f944971f47acd
Thanh Le <thanhlevan0203@gmail.com>
2022-10-15 00:27:04 -0700

    Fetch Exercise Detail Component

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -3,14 +3,25 @@ import React from 'react'
 import BodyPartImage from '../assets/icons/body-part.png';
 import TargetImage from '../assets/icons/target.png';
 import EquipmentImage from '../assets/icons/equipment.png';
-import { Stack } from '@mui/system';
+import { Stack, Typography } from '@mui/material';
+
 
 const Detail = ({ exerciseDetail }) => {
   const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
 
+
   return (
     <Stack gap="60px" sx={{ flexDirection: { lg: 'row'}, p: '20px', alignItems: 'center'}}>
       <img src={gifUrl} alt={name} loading="lazy" className='detail-image'/>
+      <Stack sx={{ gap: { lg: '35px', xs: '20px' }}}>
+        <Typography>
+            {name}
+        </Typography>
+        <Typography>
+            Exercises keep you strong. 3/4 Sit-up bup is one of the best exercises to target your abs.
+            It will help you improve your mood and gain energy.
+        </Typography>
+      </Stack>
     </Stack>
   )
 }
