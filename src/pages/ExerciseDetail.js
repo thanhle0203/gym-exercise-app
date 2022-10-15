f6862685ce8a8c37b51fc00c49bd59cb6b52e956
Thanh Le <thanhlevan0203@gmail.com>
2022-10-15 00:01:39 -0700

    Create Exercise Detail Component

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,6 +1,6 @@
 
 import { Box } from '@mui/system';
-import React, {useState } from 'react'
+import React, {useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
 import Detail from '../components/Detail';
 import ExerciseVideos from '../components/ExerciseVideos';
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -7,6 +7,14 @@ export const exerciseOptions = {
         }
 };
 
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '99db9032f2mshb0ea01c4f918384p1acfb6jsnaec49441eafe',
+  },
+};
+
 export const fetchData = async (url, options) => {
     const response = await fetch(url, options);
     const data = await response.json();
