15464b192f91f888b61a3669dded6c90b0830c98
Thanh Le <thanhlevan0203@gmail.com>
2022-10-17 22:56:32 -0700

    Fetch Exercise Video

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -5,8 +5,28 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
   return (
     <Box sx={{ marginTop: { lg: '200px', xs: '20px' }}} p="20px">
       <Typography variant='h3' mb='33px'>
-        Watch {name} exercise videos
+        Watch <span style={{color: '#ff2625', textTransform: 'capitalize'}}>{name}</span> 
+        exercise videos
       </Typography>
+
+      <Stack justifyContent='flex-start' flexWrap='wrap' alignItems='center'
+        sx={{
+            flexDirection: { lg: 'row' },
+            gap: { lg: '110px', xs: '0' }
+        }}
+      >
+        {exerciseVideos?.slice(0,3).map((item, index) => (
+            <a
+              key={index}
+              className="exercise-video"
+              href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
+              target="_blank"
+              rel='noreferrer'
+            >
+              <img src={item.video.thumbnails[0]} alt={item.video.title}  loading='lazy' />
+            </a>
+        ))}
+      </Stack>
     </Box>
   )
 }
