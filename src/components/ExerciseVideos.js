eea0d8a3f0dc6c06edbdab1364eff96473a87d8b
Thanh Le <thanhlevan0203@gmail.com>
2022-10-17 22:45:28 -0700

    Create Exercise Video

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Box, Stack, Typography } from '@mui/material';
 
-const ExerciseVideos = () => {
+const ExerciseVideos = ({ exerciseVideos, name }) => {
   return (
-    <div>ExerciseVideos</div>
+    <Box sx={{ marginTop: { lg: '200px', xs: '20px' }}} p="20px">
+      <Typography variant='h3' mb='33px'>
+        Watch {name} exercise videos
+      </Typography>
+    </Box>
   )
 }
 
