1d32b9b54d530fc177d074d8405887ac9d587821
Thanh Le <thanhlevan0203@gmail.com>
2022-10-18 23:54:04 -0700

    create footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box mt='80px' bgcolor='#fff3f4'>
+      <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlO4usncyzTjHVOYxWAsqFLepfsKKOgpoPRtYTgokxFJlY7KmLHCWevV84npPnfCxPLyE&usqp=CAU' alt='logo' width='200px' height='40px' />
+        <Typography variant='h5' pb='40px' mt='20px'>
+          Made by with love by Thanh Le
+        </Typography>
+      </Stack>
+    </Box>
   )
 }
 
--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -6,12 +6,19 @@ import Loader from './Loader'
 const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
   return (
     <Box sx={{ mt: { lg: '100px', xs: '0' }}}>
-      <Typography variant='h3'>Exercises that target the same muscle group</Typography>
+      <Typography variant='h3' mb={5}>Exercises that target the same muscle group</Typography>
       <Stack direction="row" sx={{ p:'2', position: 'relative' }}>
         {targetMuscleExercises.length ? <HorizontalScrollbar data={targetMuscleExercises} />
         : <Loader/>}
 
       </Stack>
+
+      <Stack direction="row" sx={{ p:'2', position: 'relative' }}>
+        {equipmentExercises.length ? <HorizontalScrollbar data={equipmentExercises} />
+        : <Loader/>}
+
+      </Stack>
+
     </Box>
   )
 }
