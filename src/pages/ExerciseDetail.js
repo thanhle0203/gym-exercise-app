4be02e181d27b7a765d3de1666cb0a31c7891569
Thanh Le <thanhlevan0203@gmail.com>
2022-10-18 23:05:15 -0700

    Create exercise video component

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -12,6 +12,8 @@ import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
   const [exerciseVideos, setExerciseVideos] = useState({});
+  const [targetMuscleExercises, setTargetMuscleExercises] = useState({});
+  const [equipmentExercises, setEquipmentExercises] = useState({});
   const { id } = useParams();
 
   useEffect(() => {
@@ -25,6 +27,14 @@ const ExerciseDetail = () => {
       const exerciseVideoData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`, youtubeOptions);
       setExerciseVideos(exerciseVideoData.contents);
 
+      const targetMuscleExercisesData = await fetchData(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`, 
+      exerciseOptions);
+      setTargetMuscleExercises(targetMuscleExercisesData);
+
+      const equipmentExercisesData = await fetchData(`${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`, 
+      exerciseOptions);
+      setEquipmentExercises(equipmentExercises);
+      
     }
     fetchExercisesData();
   }, [id]);
